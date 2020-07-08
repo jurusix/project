cd4529365f1bbaa0d6bc415acdfd45ff6e63c1b9
Juraj Jakubik <juraj.jakubik@tieto.com>
2020-07-08 10:49:00 +0200

    Workaround test 7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   googleLoginProvider: '989994680832-tmocfbqfcbb9r1pogpn953tuqqcvl71c.apps.googleusercontent.com',
-  redirectUri: '/oauth2callback.html'
+  redirectUri: '/oauth2callback.html?'
 };
